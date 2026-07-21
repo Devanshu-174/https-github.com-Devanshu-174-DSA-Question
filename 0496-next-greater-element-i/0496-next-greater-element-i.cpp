a8f0b8471d56c6fa58994fc4a36b7db696fa8592
Devanshu-174 <kumawatdevanshu08@gmail.com>
2026-07-21 21:25:49 +0530

    Time: 0 ms (100%), Space: 12.8 MB (39.96%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -4,22 +4,25 @@ public:
         stack<int> st;
         unordered_map<int, int> mp;
         vector<int> ans;
-        for(int i=0;i<nums2.size();i++){
-            while(!st.empty() && nums2[i]>nums2[st.top()]){
-                mp[nums2[st.top()]]=nums2[i];
+
+        for (int i = 0; i < nums2.size(); i++) {
+            while (!st.empty() && nums2[i] > nums2[st.top()]) {
+                mp[nums2[st.top()]] = nums2[i];
                 st.pop();
             }
             st.push(i);
         }
 
-        for(int i=0;i<nums1.size();i++){
-            if(mp[nums1[i]]==0){
-                ans.push_back(-1);
-            }
-            else{
-            ans.push_back(mp[nums1[i]]);
-            }
+        // Elements left in the stack have no next greater element
+        while (!st.empty()) {
+            mp[nums2[st.top()]] = -1;
+            st.pop();
         }
+
+        for (int i = 0; i < nums1.size(); i++) {
+            ans.push_back(mp[nums1[i]]);
+        }
+
         return ans;
     }
 };
